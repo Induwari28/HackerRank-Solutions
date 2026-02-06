b337f741f172568b6261cdb546be2216c75d8906
induwari senewirathne <upamalisenewirathne@gmail.com>
2026-02-06 17:37:19 +0530

    solve me first done

---
+++ Algorithms/Solve Me First.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int a, b;
+
+   
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+
+    printf("%d\n", sum(a, b));
+
+    return 0;
+}
